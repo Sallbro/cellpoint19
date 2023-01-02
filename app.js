8288244f3f4cfa81ae6282bbf08af96a9216b2f6
salman <asusalman986@gmail.com>
2023-01-02 10:19:52 +0530

    11 commit

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ const port = process.env.PORT || 5000;
 const rout = app.use(require('./routers/routs'));
 
 // vercel
-if (process.env.NODE_ENV == "production") {
-    app.use(express.static(path.resolve(__dirname, 'cellpoint', 'build')));
-    res.sendFile(path.resolve(__dirname, 'cellpoint', 'build', 'index.html'));
+if (process.env.NODE_ENV == 'production') {
+    app.use(express.static(path.resolve(__dirname, './cellpoint', 'build')));
+    app.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, './cellpoint', 'build', 'index.html'));
+    });
 }
 
 app.listen(port, () => {
